188fde13d66b06643838963a1b88e984cb3115d7
mervealgi <mervealgi@gmail.com>
2021-02-09 00:33:02 +0300

    app.js added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-// CODE EXPLAINED channel
+//ELEMENTS
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+//CLASSES
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+//TODAY S DATE
+const options = {weekday: "long", month: "short", day:"numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US",options);
